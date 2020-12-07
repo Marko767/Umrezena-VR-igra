d7262cdd130c0d1027edffc60e180488cd39490c
nsunjo <nikola.sunjo@gmail.com>
2020-12-07 17:13:46 +0100

    Nova scena

---
+++ VR_PingPong/Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    int playerScore;
+    public bool playing = true;
+    public AudioSource hit;
+
+    [SerializeField] Text player1_score_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = 0;
+        hit = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Paddle"))
+        {
+            hit.Play();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Out"))
+        {
+            playerScore++;
+            //playing = false;
+            updateScores();
+        }
+    }
+    void updateScores()
+    {
+        player1_score_text.text = "Player 1 : " + playerScore;
+    }
+}
---
+++ VR_PingPong/Assets/pfizika.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pfizika : MonoBehaviour
+{
+    private Rigidbody rigbody;
+    private Vector3 brzina;
+    public float sens = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dest = this.transform.position;
+        rigbody.transform.rotation = transform.rotation;
+        brzina = (dest - rigbody.transform.position) * sens;
+        rigbody.velocity = brzina;
+        //uzmi poziciju ovog trenutka
+        //izračunaj (trenutna - prijašnja) pozicija
+        //izračunaj kao vektor smjera
+        //predaj taj vektor loptici
+        //prijašnja pozicija = trenutna pozicija
+
+    }
+}
